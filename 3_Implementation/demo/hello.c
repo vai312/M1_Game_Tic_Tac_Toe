80d9e6c0d0b08ad27222480fdadc84e62213d4ff
vai312 <94313273+vai312@users.noreply.github.com>
2021-11-23 16:22:34 +0530

    Update hello.c

--- 3_Implementation/demo/hello.c
+++ 3_Implementation/demo/hello.c
@@ -3,6 +3,8 @@ int main()
 {
     printf("Welcome to Github Actions\n");
     printf("\n******Hurry, It works******\n");
+     printf("\n******Hurry, It works******\n");
+     printf("\n******Hurry, It works******\n");
     return 0;
 }
 
