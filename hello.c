bff38adfab71e4e96d40870c4b6982c157d1345f
vai312 <94313273+vai312@users.noreply.github.com>
2021-11-23 16:39:42 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,1 +1,10 @@
 
+#include <stdio.h>
+int main()
+{
+    printf("Welcome to Github Actions\n");
+    printf("\n******Hurry, It works******\n");
+     printf("\n******Hurry, It works******\n");
+     printf("\n******Hurry, It works******\n");
+    return 0;
+}
